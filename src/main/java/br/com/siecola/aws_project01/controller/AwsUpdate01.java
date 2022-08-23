c492358d343c754e0a5355e0dc78badd78d53468
adcorrea <antoniopucc@hotmail.com>
2022-08-22 21:49:45 -0300

    projeto criado

---
+++ src/main/java/br/com/siecola/aws_project01/controller/AwsUpdate01.java
@@ -0,0 +1,23 @@
+package br.com.siecola.aws_project01.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/awsupdate")
+public class AwsUpdate01 {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/hello/{name}")
+    public ResponseEntity<?> awsTest(@PathVariable String name){
+        LOG.info("Aws Update controller - name: {}", name);
+
+        return ResponseEntity.ok("Hello: " + name);
+
+    }
+}
